8c1a23ff1b9a7cc9a5902bbbf64b1211f9961162
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2020-12-18 11:33:06 +0200

    Muokattu .js tiedostoa

--- maat.js
+++ maat.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
@@ -76,9 +77,4 @@ const App = () => {
     </div>
   )
 }
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById('root'))
